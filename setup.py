7c43431d3e60c2ca628af3eaac336d063aa2f17b
Didier Bröska <didier.broska@gmail.com>
2020-06-12 08:29:53 +0200

    Upgrade to setup.cfg

--- setup.py
+++ setup.py
@@ -1,34 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
 """
-Setup module
+Setup script
 """
-from os import path
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
-
-setup(
-    name="pyedid",
-    version="0.1",
-    packages=find_packages(),
-    entry_points={"console_scripts": ["pyedid = pyedid.main:main"]},
-    author="Jonas Lieb",
-    author_email="",
-    maintainer="Davydov Denis",
-    maintainer_email="dadmoscow@gmail.com",
-    url="https://github.com/dadmoscow/pyedid",
-    description="This is a python library to parse extended display identification data (EDID)",
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    keywords="edid xrandr display",
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: POSIX :: Linux",
-        "Programming Language :: Python :: 3",
-        "Topic :: Software Development :: Libraries",
-        "Topic :: System :: Hardware",
-    ]
+setup_kwargs = dict(
+    test_suite='tests'
 )
+
+try:
+    setup(
+            use_scm_version=True,
+            **setup_kwargs
+    )
+except LookupError:
+    # This means the source code was not from git / PyPI
+    setup(
+            version='0.0.3'
+            **setup_kwargs
+    )
+
